8d72b456021c002d48efbec08682996698f50a33
Fernando Flores <fflores10@uabc.edu.mx>
2017-03-12 19:33:31 -0700

    first commit

---
+++ src/ImageButton.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fernando 
+ */
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+class ImageButton extends JButton {
+
+    private boolean isLastButton;
+
+    public ImageButton() {
+
+        super();
+
+        initUI();
+    }
+
+    public ImageButton(Image image) {
+
+        super(new ImageIcon(image));
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        isLastButton = false;
+        BorderFactory.createLineBorder(Color.gray);
+
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setBorder(BorderFactory.createLineBorder(Color.yellow));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBorder(BorderFactory.createLineBorder(Color.gray));
+            }
+        });
+    }
+
+    public void setLastButton() {
+        
+        isLastButton = true;
+    }
+
+    public boolean isLastButton() {
+
+        return isLastButton;
+    }
+}
